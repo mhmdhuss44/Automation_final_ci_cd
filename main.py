c7fd50bba378ff660efa8bb72434d3ea5342eaa6
mhmdhuss <mhmdhuss44@gmail.com>
2024-03-22 12:10:06 +0200

    updated the main script (test runner)

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import unittest
-from infra_layer.infra_ui.wrapper import browserWrapper
+import pytest
 from testing_layer.api_tests.add_area_test import AreaTests
+from infra_layer.infra_ui.wrapper import browserWrapper
 from testing_layer.ui_tests.check_area_test import areaTests
 
+@pytest.fixture
+def infra_layer():
+    return browserWrapper()
 
 def test_run_grid_serial(infra_layer):
     print(infra_layer.cab_list)
     for cabs in infra_layer.cab_list:
-        # Instantiate the searchTests class
+        # Instantiate the areaTests class
         search_test_instance = areaTests()
         # Pass any necessary parameters to the test method
         search_test_instance.verify_successful_area_add(cabs)
-        # You may add additional test methods from searchTests if needed
-
+        # You may add additional test methods from areaTests if needed
 
 if __name__ == "__main__":
-    infra_layer = browserWrapper()
-    configs = infra_layer.get_all_configurations()
-    test_run_grid_serial(infra_layer)
+    pytest.main([__file__])
